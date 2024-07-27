cfc1a20d8145d9356de90896a4f68a33f306a6ac
fuZail-1 <135430030+fuZail-1@users.noreply.github.com>
2024-07-27 14:29:55 +0530

    Delete weatherapp directory

--- weatherapp/src/config/app-constant.js
+++
@@ -1,9 +0,0 @@
-export const WEATHER_API_KEY = "8f983bb0968d49488dd60837242707"; //key from .eny file
-
-export const DEFAULT_CITY_NAME = "mumbai";
-export const WEATHER_API_BASE_URL = "https://api.weatherapi.com/v1/";
-
-export const TABS = {
-  FORCAST: "forcast",
-  CURRENT_WEATHER: "current weather",
-};
--- weatherapp/src/redux/configureStore.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import searchSlice from "./weatherDetailsSlice";
-export default configureStore({
-  reducer: {
-    searchSlice,
-  },
-});
